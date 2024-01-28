da4bef74fe3ff5c88656349175cd6f7283c05868
Daryna Holikova <dgolikova0@gmail.com>
2024-01-28 00:52:35 +0000

    add proper cloudinary config

--- middleware/uploadAvatar.js
+++ middleware/uploadAvatar.js
@@ -16,9 +16,9 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 cloudinary.config({ 
-  cloud_name: 'dgj3zunfg', 
-  api_key: '692376826663911', 
-  api_secret: 'fTTNZr_bpLUBDGdJP6N8C11jkBM' 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.API_KEY, 
+  api_secret: process.env.API_SECRET
 });
 
 const uploadAvatar = async (req, res, next) => {
