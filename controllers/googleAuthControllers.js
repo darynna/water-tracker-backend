0432314d67c0acfa9e62dbf98e46a6e2a1e8a989
Daryna Holikova <dgolikova0@gmail.com>
2024-02-02 12:57:05 +0000

    changes

--- controllers/googleAuthControllers.js
+++ controllers/googleAuthControllers.js
@@ -1,4 +1,4 @@
-const queryString = require("querystring")
+const queryString = require("query-string");
 const axios = require("axios")
 const {googleAuthServer} = require('../db/services/googleAuthServices')
 
@@ -20,37 +20,36 @@ exports.googleAuth = async (req, res) => {
     );
   };
   
-//   ${process.env.BACKEND_URL}
 
-exports.goodleRedirect = async (req, res) => {
-    const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
-    const urlObj = new URL(fullUrl);
-    const urlParams = queryString.parse(urlObj.search);
-    const code = urlParams.code;
-  
-    const tokenData = await axios({
-      url: "https://oauth2.googleapis.com/token",
-      method: "post",
-      data: new URLSearchParams({
-        client_id: process.env.GOOGLE_CLIENT_ID,
-        client_secret: process.env.GOOGLE_CLIENT_SECRET,
-        redirect_uri: `http://localhost:5001/api/user/google-redirect`,
-        grant_type: "authorization_code",
-        code,
-      }),
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    });
-  
-    const userData = await axios({
-      url: "https://www.googleapis.com/oauth2/v2/userinfo",
-      method: "get",
-      headers: {
-        Authorization: `Bearer ${tokenData.data.access_token}`,
-      },
-    });
-    const token = await googleAuthServer(userData.data);
+exports.googleRedirect = async (req, res) => {
+  const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
+  const urlObj = new URL(fullUrl);
+  const urlParams = queryString.parse(urlObj.search);
+  const code = urlParams.code;
+  const tokenData = await axios({
+    url: `https://oauth2.googleapis.com/token`,
+    method: "post",
+    data: {
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      redirect_uri: `http://localhost:5001/api/user/google-redirect`,
+      grant_type: "authorization_code",
+      code,
+    },
+  });
+  const userData = await axios({
+    url: "https://www.googleapis.com/oauth2/v2/userinfo",
+    method: "get",
+    headers: {
+      Authorization: `Bearer ${tokenData.data.access_token}`,
+    },
+  });
+      const token = await googleAuthServer(userData.data);
   
     return res.redirect(`${process.env.BASE_URL}?token=${token}`);
-  };
+  // return res.redirect(
+  //   `${process.env.FRONTEND_URL}?email=${userData.data.email}`
+  // );
+};
+
+//   ${process.env.BACKEND_URL}
