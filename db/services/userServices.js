d913f155ea8a6e37f572f838164963173537b788
Viktoriia-Parpalii <127688031+Viktoriia-Parpalii@users.noreply.github.com>
2024-02-06 22:29:44 +0200

    Update userServices.js

--- db/services/userServices.js
+++ db/services/userServices.js
@@ -26,7 +26,49 @@ const createUser = async (body) => {
   const verifyEmail = {
     to: email,
     subject: "Verify email",
-    html: `<a target="_blank" href="${BACKEND_URL}/api/user/verify/${verificationToken}">Click to verify your e-mail</a>`,
+    html: `  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+        }
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+        h2 {
+            color: #3498db;
+        }
+        p {
+            color: #555;
+        }
+        a {
+            display: inline-block;
+            margin-top: 20px;
+            padding: 10px 20px;
+            background-color: #3498db;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h2>Email Confirmation</h2>
+        <p>Thank you for registering. Please click the link below to confirm your email address:</p>
+       <a target="_blank" href="${BACKEND_URL}/api/user/verify/${verificationToken}">Click to verify your e-mail</a>
+    </div>
+</body>
+</html>
+    
+   `,
   };
 
   await sendEmail(verifyEmail);
@@ -55,7 +97,49 @@ const resendVerifyEmailService = async (email) => {
   const verifyEmail = {
     to: email,
     subject: "Verify email",
-    html: `<a target="_blank" href="${BACKEND_URL}/api/user/verify/${user.verificationToken}">Click to verify your e-mail</a>`,
+    html: `  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+        }
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+        h2 {
+            color: #3498db;
+        }
+        p {
+            color: #555;
+        }
+        a {
+            display: inline-block;
+            margin-top: 20px;
+            padding: 10px 20px;
+            background-color: #3498db;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h2>Email Confirmation</h2>
+        <p>Thank you for registering. Please click the link below to confirm your email address:</p>
+       <a target="_blank" href="${BACKEND_URL}/api/user/verify/${user.verificationToken}">Click to verify your e-mail</a>
+    </div>
+</body>
+</html>
+    
+   `,
   };
   await sendEmail(verifyEmail);
 };
